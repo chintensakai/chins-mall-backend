b922eb5f0d2fb322d64d471cca6a6a2672b70ca9
toyranger <toy_shen@163.com>
2019-12-22 23:04:11 +0800

    chins:oauth2密码模式测试通过，令牌存入redis

--- security/pwd-credentials/src/main/java/com/chins/mall/backend/security/config/WebSecurityConfiguration.java
+++ security/pwd-credentials/src/main/java/com/chins/mall/backend/security/config/WebSecurityConfiguration.java
@@ -1,17 +1,49 @@
 package com.chins.mall.backend.security.config;
 
+import com.chins.mall.backend.security.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+//@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+  @Bean
+  @Override
+  public UserDetailsService userDetailsServiceBean() throws Exception {
+    return new UserDetailsServiceImpl();
+  }
+
+  //  用于支持password模式
+  @Bean
+  public AuthenticationManager authenticationManager() throws Exception {
+    return super.authenticationManager();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsServiceBean());
+  }
+
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//
+//    http.csrf().disable().authorizeRequests()
+//        .anyRequest().permitAll();
+//  }
+
 }
--- security/pwd-credentials/src/main/java/com/chins/mall/backend/security/service/UserDetailsServiceImpl.java
+++ security/pwd-credentials/src/main/java/com/chins/mall/backend/security/service/UserDetailsServiceImpl.java
@@ -1,6 +1,10 @@
 package com.chins.mall.backend.security.service;
 
+import com.google.common.collect.Lists;
+import java.util.List;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,8 +14,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+  private static final String USERNAME = "admin";
+  private static final String PASSWORD = "$2a$10$Ji7b2tJSmKClopQdsj0gL.T3fc1fxN4g7h1RG3PJsjMoonAF198QS";
+
   @Override
-  public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-    return null;
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    List<GrantedAuthority> grantedAuthorityList = Lists.newArrayList();
+    return new User(USERNAME, PASSWORD, grantedAuthorityList);
   }
 }

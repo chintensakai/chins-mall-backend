cc89ee318a936dde51463fa6a795fb1927dea567
toyranger <toy_shen@163.com>
2020-01-14 21:38:06 +0800

    chins: 加入网关gateway/oauth2注册到nacos提供rest服务/通过网关路由到oauth2完成用户认证

---
+++ gateway/src/main/java/com/chins/mall/backend/gateway/GatewayApplication.java
@@ -0,0 +1,12 @@
+package com.chins.mall.backend.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+}
